662457f581874c69ec1cafca3f8bd0835459a6ec
Hunter Wallen <hunterwallen67@gmail.com>
2020-12-23 09:58:39 -0700

    created ejs for nav bar complete with working link and get route to users page

--- controllers/loggedin.js
+++ controllers/loggedin.js
@@ -12,7 +12,6 @@ const isAuthenticated = (req, res, next) => {
 }
 
 loggedIn.get('/', isAuthenticated, (req, res) => {
-  console.log(req.session.currentUser);
   User.find({}, (err, allUsers) => {
     res.render('homepage.ejs', {
       allUsers: allUsers,
@@ -21,11 +20,20 @@ loggedIn.get('/', isAuthenticated, (req, res) => {
   })
 });
 
+loggedIn.get('/userpage/:id', (req, res) => {
+  User.findById( req.params.id, (err, foundUser) => {
+    res.render('userpage.ejs', {
+      user: foundUser,
+      currentUser: req.session.currentUser
+    })
+  })
+})
+
 loggedIn.post('/newpost/:id', (req, res) => {
   let userID = req.params.id
   User.findById(userID, (err, foundUser) => {
     Post.create(req.body, (err, createdPost) => {
-      foundUser.post.push(createdPost)
+      foundUser.post.unshift(createdPost)
       foundUser.save((err, data) => {
         res.redirect('/')
       })
